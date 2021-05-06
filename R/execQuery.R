e535867644cdb4910445942b7e54cc04c6ee1ae2
IulianD <iulian_dragan@yahoo.com>
2021-05-06 12:16:05 +0200

    default resource in execQuery

--- R/execQuery.R
+++ R/execQuery.R
@@ -1,10 +1,22 @@
-execQuery <- function(qDomain, qName, qInput, resource){
+execQuery <- function(qDomain, qName, qInput, resource = NULL){
   allq <- get('allQueries', envir = .queryLibrary)
   qList <- allq[[qDomain]]
   realQname <- grep(qName, names(qList), value = TRUE)
   myQuery <- qList[[realQname]]$Query
 
   qInput <- dsSwissKnife:::.decode.arg(qInput)
+  if(is.null(resource)){
+    for (i in ls(envir = parent.frame())){
+      x <- get(i, envir = parent.frame())
+      if("SQLFlexClient" %in% class(x)){
+        resource <- x
+        break
+      }
+    }
+  }
+  if(is.null(resource)){
+    stop('Could not find a suitable database connection.')
+  }
 
 # must be set via option:
   myQuery <- gsub('@cdm', getOption('cdm_schema'), myQuery, fixed = TRUE)
